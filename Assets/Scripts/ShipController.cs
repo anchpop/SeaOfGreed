d3ee144060b4b853ee986760495f77385d0ef235
anchpop <andre@andreisawesome.com>
2017-07-03 16:59:04 -0400

    added s for brake

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -7,6 +7,7 @@ public class ShipController : MonoBehaviour {
     Rigidbody2D body;
 
     public float acceleration = 5;
+    public float breakspeed = 2;
     public float maxspeed = 10;
     public float torque = 2;
     // Use this for initialization
@@ -14,14 +15,14 @@ public class ShipController : MonoBehaviour {
         body = GetComponent<Rigidbody2D>();
     }
 
-    private void applyContinuousForce(float speed, Vector3 force, float maxVelocity)
+    private void applyContinuousForce(float speed, Vector3 force, float maxVelocity, float minVelocity = 0)
     {
         Vector2 forceToApply = speed * force;
         Vector2 velocityChange = forceToApply * Time.fixedDeltaTime;
         float velocityInDirection = Vector3.Dot(body.velocity, force.normalized);
         float velocityInDirectionAfterForce = Vector3.Dot(body.velocity + velocityChange, force.normalized);
 
-
+                
         if (Mathf.Abs(velocityInDirectionAfterForce) < maxVelocity || Mathf.Sign(velocityInDirectionAfterForce) < 0) // if we're moving in the opposite direction of movement
             body.AddForce(forceToApply, ForceMode.Acceleration);
         else if (Mathf.Abs(velocityInDirection) < maxVelocity) // if the speed addition wouldn't make us accelerate past the max run speed
@@ -31,11 +32,17 @@ public class ShipController : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update () { 
         if (Input.GetKey(KeyCode.W)) {
             Debug.Log("Adding force");
-            applyContinuousForce(5.0f, transform.up, 10);
+            applyContinuousForce(acceleration, transform.up, maxspeed);
         }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("Adding force");
+            applyContinuousForce(breakspeed, -transform.up, 0);
+        }
+
         if (Input.GetKey(KeyCode.A))
         {
             Debug.Log("Adding torque");
