c7c532eeebaaa63b1b23959fba50057cff968f7a
anchpop <andre@andreisawesome.com>
2017-07-04 00:51:31 -0400

    Added wheel grabbing!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,8 +10,12 @@ public class PlayerController : MonoBehaviour {
     public float boardShipRange = 10;
     public float dockShipRange = 10;
 
+    public float minDistanceToGrabWheel = .5f;
+
     bool boarded = false;
     GameObject shipBorded;
+
+    bool wheelGrabbed = false;
 
     public LayerMask walkRaycastMask;
     public LayerMask dockRaycastMask;
@@ -32,6 +36,17 @@ public class PlayerController : MonoBehaviour {
         return Vector3.Distance(transform.position, shipBorded.GetComponent<ShipController>().wheelMarker.transform.position);
     }
 
+    void grabWheel()
+    {
+        wheelGrabbed = true;
+        transform.position = shipBorded.GetComponent<ShipController>().wheelMarker.transform.position;
+    }
+
+    void releaseWheel()
+    {
+        wheelGrabbed = false;
+    }
+
     void boardShip(GameObject ship)
     {
         transform.position = ship.transform.position;
@@ -46,7 +61,6 @@ public class PlayerController : MonoBehaviour {
         RaycastHit2D boatFound = boatSearch(1);
         if (!shipBorded && boatFound)
         {
-            Debug.Log("boatFound");
             if (Input.GetKeyDown(KeyCode.V))
             {
                 boardShip(boatFound.collider.gameObject);
@@ -54,28 +68,61 @@ public class PlayerController : MonoBehaviour {
         }
         if (shipBorded)
         {
-            getDistanceToWheel();
+            if (!wheelGrabbed && getDistanceToWheel() < minDistanceToGrabWheel)
+            {
+                if (Input.GetKeyDown(KeyCode.E))
+                {
+                    grabWheel();
+                }
+            }
+            else if (wheelGrabbed && Input.GetKeyDown(KeyCode.E))
+            {
+                releaseWheel();
+            }
         }
 
-        var input_x = Input.GetAxisRaw("Horizontal"); 
-        var input_y = Input.GetAxisRaw("Vertical");
+        if (wheelGrabbed)
+        {
+            var shipController = shipBorded.GetComponent<ShipController>();
+            if (Input.GetKey(KeyCode.W))
+            {
+                shipController.accelerate();
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                shipController.brake();
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                shipController.turn(1);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                shipController.turn(-1);
+            }
+        }
+        else
+        {
+            var input_x = Input.GetAxisRaw("Horizontal");
+            var input_y = Input.GetAxisRaw("Vertical");
 
-        
-        bool isWalking = (input_x != 0) || (input_y != 0);
 
-        var xToOffset = transform.right * input_x;
-        var yToOffset = transform.up * input_y;
+            bool isWalking = (input_x != 0) || (input_y != 0);
 
-        // TODO - shoot 2 rays for each dir, one for each corner, according to Sprite.bounds
-        // This way the player won't be able to slide past some walls
-        RaycastHit2D x_ray = Physics2D.Raycast(transform.position + xToOffset/10, xToOffset, width, walkRaycastMask); 
-        RaycastHit2D y_ray = Physics2D.Raycast(transform.position + yToOffset/10, yToOffset, height, walkRaycastMask);
-        //Debug.DrawRay(transform.position, xToOffset/50  , Color.green);
-        var xOffset = (x_ray) ? xToOffset : Vector3.zero;
-        var yOffset = (y_ray) ? yToOffset : Vector3.zero;
+            var xToOffset = transform.right * input_x;
+            var yToOffset = transform.up * input_y;
+
+            // TODO - shoot 2 rays for each dir, one for each corner, according to Sprite.bounds
+            // This way the player won't be able to slide past some walls
+            RaycastHit2D x_ray = Physics2D.Raycast(transform.position + xToOffset / 10, xToOffset, width, walkRaycastMask);
+            RaycastHit2D y_ray = Physics2D.Raycast(transform.position + yToOffset / 10, yToOffset, height, walkRaycastMask);
+            //Debug.DrawRay(transform.position, xToOffset/50  , Color.green);
+            var xOffset = (x_ray) ? xToOffset : Vector3.zero;
+            var yOffset = (y_ray) ? yToOffset : Vector3.zero;
 
 
-        if (isWalking)
-            transform.position += ((xOffset ) + (yOffset)).normalized * walkSpeed * Time.deltaTime;
+            if (isWalking)
+                transform.position += ((xOffset) + (yOffset)).normalized * walkSpeed * Time.deltaTime;
+        }
     }
 }
