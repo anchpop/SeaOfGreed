5f19f2cf2a7dd186b5516dd292867f83b8a28389
anchpop <andre@andreisawesome.com>
2017-07-04 20:01:29 -0400

    camera zoomout when steering, check

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,206 +4,217 @@ using UnityEngine.Assertions;
 using UnityEngine;
 
 namespace SeaOfGreed{
-public class PlayerController : MonoBehaviour {
-    enum states
-    {
-        noState,
-        onLand,
-        swimming,
-        boardedShip,
-        steeringShip,
-    }
-    states state = states.onLand;
-    states newState = states.noState;
-
-    public float walkSpeed = 2;
-    public float width = .12f;
-    public float height = .12f;
-
-    public float boardShipRange = 10;
-    public float dockShipRange = 10;
-
-    public float minDistanceToGrabWheel = .5f;
-
-    public LayerMask walkRaycastMask;
-    public LayerMask dockRaycastMask;
-    public LayerMask boatRaycastMask;
-
-    public GameObject boardText;
-    public GameObject dockText;
-    public GameObject wheelText;
-
-    Animator anim;
-    
-    GameObject shipBorded;
-    
-
-
-
-    // Use this for initialization
-    void Start () {
-		Keybindings.Load ();
-        anim = GetComponent<Animator>();
-	}
-
-    RaycastHit2D boatSearch(int iterations)
-    {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, boardShipRange, boatRaycastMask);
-        return hit;
-    }
-    RaycastHit2D dockSearch(int iterations)
-    {
-        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left, boardShipRange, dockRaycastMask);
-        return hit;
-    }
-
-    float getDistanceToWheel()
-    {
-        return Vector3.Distance(transform.position, shipBorded.GetComponent<ShipController>().wheelMarker.transform.position);
-    }
-
-    void walkAccordingToUserInput()
-    {
-        var input_x = Input.GetAxisRaw("Horizontal");
-        var input_y = Input.GetAxisRaw("Vertical");
-
-
-        bool isWalking = (input_x != 0) || (input_y != 0);
-
-        var xToOffset = transform.right * input_x;
-        var yToOffset = transform.up * input_y;
-
-        // TODO - shoot 2 rays for each dir, one for each corner, according to Sprite.bounds
-        // This way the player won't be able to slide past some walls
-        RaycastHit2D x_ray = Physics2D.Raycast(transform.position + xToOffset / 10, xToOffset, width, walkRaycastMask);
-        RaycastHit2D y_ray = Physics2D.Raycast(transform.position + yToOffset / 10, yToOffset, height, walkRaycastMask);
-        //Debug.DrawRay(transform.position, xToOffset/50  , Color.green);
-        var xOffset = (x_ray) ? xToOffset : Vector3.zero;
-        var yOffset = (y_ray) ? yToOffset : Vector3.zero;
-
-
-        if (isWalking)
-            transform.position += ((xOffset) + (yOffset)).normalized * walkSpeed * Time.deltaTime;
-    }
-
-    // Update is called once per frame
-    void Update () {
-        if (newState != states.noState)
+    public class PlayerController : MonoBehaviour {
+        enum states
         {
-            state = newState;
-            newState = states.noState;
+            noState,
+            onLand,
+            swimming,
+            boardedShip,
+            steeringShip,
+        }
+        states state = states.onLand;
+        states newState = states.noState;
+
+        public float walkSpeed = 2;
+        public float width = .12f;
+        public float height = .12f;
+
+        public float boardShipRange = 10;
+        public float dockShipRange = 10;
+
+        public float minDistanceToGrabWheel = .5f;
+
+        public LayerMask walkRaycastMask;
+        public LayerMask dockRaycastMask;
+        public LayerMask boatRaycastMask;
+
+        public GameObject boardText;
+        public GameObject dockText;
+        public GameObject wheelText;
+
+        public Camera mainCamera;
+        public float walkingCameraSize = 4;
+        public float interactingCameraSize = 6;
+        public float cameraEaseTime = 2;
+
+        Animator anim;
+    
+        GameObject shipBorded;
+    
+    
+
+
+
+        // Use this for initialization
+        void Start () {
+		    Keybindings.Load ();
+            anim = GetComponent<Animator>();
+	    }
+
+        RaycastHit2D boatSearch(int iterations)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right, boardShipRange, boatRaycastMask);
+            return hit;
+        }
+        RaycastHit2D dockSearch(int iterations)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.left, boardShipRange, dockRaycastMask);
+            return hit;
         }
 
-        if (state == states.onLand)
+        float getDistanceToWheel()
         {
-            RaycastHit2D boatFound = boatSearch(1);
-            if (!boatFound)
+            return Vector3.Distance(transform.position, shipBorded.GetComponent<ShipController>().wheelMarker.transform.position);
+        }
+
+        void walkAccordingToUserInput()
+        {
+            var input_x = Input.GetAxisRaw("Horizontal");
+            var input_y = Input.GetAxisRaw("Vertical");
+
+
+            bool isWalking = (input_x != 0) || (input_y != 0);
+
+            var xToOffset = transform.right * input_x;
+            var yToOffset = transform.up * input_y;
+
+            // TODO - shoot 2 rays for each dir, one for each corner, according to Sprite.bounds
+            // This way the player won't be able to slide past some walls
+            RaycastHit2D x_ray = Physics2D.Raycast(transform.position + xToOffset / 10, xToOffset, width, walkRaycastMask);
+            RaycastHit2D y_ray = Physics2D.Raycast(transform.position + yToOffset / 10, yToOffset, height, walkRaycastMask);
+            //Debug.DrawRay(transform.position, xToOffset/50  , Color.green);
+            var xOffset = (x_ray) ? xToOffset : Vector3.zero;
+            var yOffset = (y_ray) ? yToOffset : Vector3.zero;
+
+
+            if (isWalking)
+                transform.position += ((xOffset) + (yOffset)).normalized * walkSpeed * Time.deltaTime;
+        }
+
+        // Update is called once per frame
+        void Update () {
+            if (newState != states.noState)
             {
-                boardText.SetActive(false);
+                state = newState;
+                newState = states.noState;
             }
-            if (boatFound)
+
+            if (state == states.onLand)
             {
-                boardText.SetActive(true);
-                if (Input.GetKeyDown(Keybindings.enterShip))
+                RaycastHit2D boatFound = boatSearch(1);
+                if (!boatFound)
                 {
-                    onLandToBoardedShip(boatFound.collider.gameObject);
+                    boardText.SetActive(false);
                 }
+                if (boatFound)
+                {
+                    boardText.SetActive(true);
+                    if (Input.GetKeyDown(Keybindings.enterShip))
+                    {
+                        onLandToBoardedShip(boatFound.collider.gameObject);
+                    }
+                }
+
+                walkAccordingToUserInput();
+            }
+            if (state == states.boardedShip)
+            {
+                var dockFound = dockSearch(1);
+            
+                var distanceToWheel = getDistanceToWheel();
+                if (distanceToWheel > minDistanceToGrabWheel)
+                {
+                    wheelText.SetActive(false);
+                }
+                else if (distanceToWheel < minDistanceToGrabWheel)
+                {
+                    wheelText.SetActive(true);
+                    if (Input.GetKeyDown(Keybindings.use))
+                    {
+                        boardedShipToSteeringShip();
+                    }
+                }
+                if (dockFound)
+                {
+                    dockText.SetActive(true);
+                    if (Input.GetKeyDown(Keybindings.enterShip))
+                    {
+                        Debug.Log(dockFound.point);
+                        boardedShipToOnLand(dockFound.point);
+                    }
+                }
+
+                walkAccordingToUserInput();
             }
 
-            walkAccordingToUserInput();
-        }
-        if (state == states.boardedShip)
-        {
-            var dockFound = dockSearch(1);
-            
-            var distanceToWheel = getDistanceToWheel();
-            if (distanceToWheel > minDistanceToGrabWheel)
+            if (state == states.steeringShip)
             {
-                wheelText.SetActive(false);
-            }
-            else if (distanceToWheel < minDistanceToGrabWheel)
-            {
-                wheelText.SetActive(true);
+                var shipController = shipBorded.GetComponent<ShipController>();
+                if (Input.GetKey(Keybindings.shipForward))
+                {
+                    shipController.accelerate();
+                }
+                if (Input.GetKey(Keybindings.shipReverse))
+                {
+                    shipController.brake();
+                }
+                if (Input.GetKey(Keybindings.shipLeft))
+                {
+                    shipController.turn(1);
+                }
+                if (Input.GetKey(Keybindings.shipRight))
+                {
+                    shipController.turn(-1);
+                }
                 if (Input.GetKeyDown(Keybindings.use))
                 {
-                    boardedShipToSteeringShip();
+                    steeringShipToBoardedShip();
                 }
             }
-            if (dockFound)
-            {
-                dockText.SetActive(true);
-                if (Input.GetKeyDown(Keybindings.enterShip))
-                {
-                    Debug.Log(dockFound.point);
-                    boardedShipToOnLand(dockFound.point);
-                }
-            }
-
-            walkAccordingToUserInput();
         }
 
-        if (state == states.steeringShip)
+        // state transitions
+        void boardedShipToSteeringShip()
         {
-            var shipController = shipBorded.GetComponent<ShipController>();
-            if (Input.GetKey(Keybindings.shipForward))
-            {
-                shipController.accelerate();
-            }
-            if (Input.GetKey(Keybindings.shipReverse))
-            {
-                shipController.brake();
-            }
-            if (Input.GetKey(Keybindings.shipLeft))
-            {
-                shipController.turn(1);
-            }
-            if (Input.GetKey(Keybindings.shipRight))
-            {
-                shipController.turn(-1);
-            }
-            if (Input.GetKeyDown(Keybindings.use))
-            {
-                steeringShipToBoardedShip();
-            }
+            Assert.IsTrue(state == states.boardedShip);
+            newState = states.steeringShip;
+            wheelText.SetActive(false);
+            transform.position = shipBorded.GetComponent<ShipController>().wheelMarker.transform.position;
+            LeanTween.cancel(mainCamera.gameObject);
+            LeanTween.value(mainCamera.gameObject, val => mainCamera.orthographicSize = val, mainCamera.orthographicSize, interactingCameraSize, cameraEaseTime).setEase(LeanTweenType.easeInOutQuad);
+            //LeanTween.value(walkingCameraSize, interactingCameraSize)
+        }
+
+        void steeringShipToBoardedShip()
+        {
+            LeanTween.cancel(mainCamera.gameObject);
+            LeanTween.value(mainCamera.gameObject, val => mainCamera.orthographicSize = val, mainCamera.orthographicSize, walkingCameraSize, cameraEaseTime).setEase(LeanTweenType.easeInOutQuad);
+
+            Assert.IsTrue(state == states.steeringShip);
+            newState = states.boardedShip;
+        }
+
+        void onLandToBoardedShip(GameObject ship)
+        {
+            Assert.IsTrue(state == states.onLand);
+            newState = states.boardedShip;
+            transform.position = ship.transform.position;
+            transform.SetParent(ship.transform);
+            shipBorded = ship;
+            boardText.SetActive(false);
+        }
+
+        void boardedShipToOnLand(Vector3 position)
+        {
+            Assert.IsTrue(state == states.boardedShip);
+            shipBorded = null;
+            transform.position = position;
+            transform.SetParent(null);
+            transform.rotation = Quaternion.identity;
+            newState = states.onLand;
+            shipBorded = null;
+            dockText.SetActive(false);
         }
     }
-
-    // state transitions
-    void boardedShipToSteeringShip()
-    {
-        Assert.IsTrue(state == states.boardedShip);
-        newState = states.steeringShip;
-        wheelText.SetActive(false);
-        transform.position = shipBorded.GetComponent<ShipController>().wheelMarker.transform.position;
-    }
-
-    void steeringShipToBoardedShip()
-    {
-        Assert.IsTrue(state == states.steeringShip);
-        newState = states.boardedShip;
-    }
-
-    void onLandToBoardedShip(GameObject ship)
-    {
-        Assert.IsTrue(state == states.onLand);
-        newState = states.boardedShip;
-        transform.position = ship.transform.position;
-        transform.SetParent(ship.transform);
-        shipBorded = ship;
-        boardText.SetActive(false);
-    }
-
-    void boardedShipToOnLand(Vector3 position)
-    {
-        Assert.IsTrue(state == states.boardedShip);
-        shipBorded = null;
-        transform.position = position;
-        transform.SetParent(null);
-        transform.rotation = Quaternion.identity;
-        newState = states.onLand;
-        shipBorded = null;
-        dockText.SetActive(false);
-
-    }
-}
 }
