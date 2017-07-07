f302ce69702699a8eaa0db6c264428d1adfcb02a
Taco <tacoguy56@users.noreply.github.com>
2017-07-07 15:47:18 -0400

    combat core
    +test attack to make sure code works

---
+++ Assets/Scripts/CombatObjects/CombatObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CombatObject : MonoBehaviour {
+	//outlines the basic properties a combat object must have.
+	[SerializeField] private int maxHP;
+	[SerializeField] private int defense;
+	private int HP;
+	void Start () {
+		HP = maxHP;
+	}
+	public int getMaxHP(){
+		return maxHP;
+	}
+	public int getDefense(){
+		return defense;
+	}
+	public int getHP(){
+		return HP;
+	}
+	public void damage(int amount){
+		if(amount>defense){
+			HP -= (amount - defense);
+			onHPChange();
+		}
+	}
+	private void onHPChange(){
+		Debug.Log("hpchange in object " + gameObject.name);
+		if(HP<=0){
+			onDeath();
+		}
+	}
+	private void onDeath(){
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Scripts/CombatObjects/PlayerCombatObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerCombatObject : CombatObject {
+	//combat object; if you hit C it spawns a projectile (controlled by damage.cs)
+	//TODO: move this control to something better when we flesh out attacking.
+	[SerializeField] private GameObject childObject;
+	private void onDeath(){
+        SceneManager.LoadScene("testscene1");
+	}
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.C))
+			childObject.SetActive(true);
+	}
+}
