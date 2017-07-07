f92dd6a70e4c705122b8983c99808f2a5b9c0614
dadur604 <dadur604604@gmail.com>
2017-07-07 10:52:59 -0700

    Added logbook, currently empty

---
+++ Assets/Scripts/LogBook.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using TeamUtility.IO;
+using UnityEngine;
+
+namespace SeaOfGreed {
+
+    public class LogBook : MonoBehaviour {
+        private Pages currentPage = Pages.none;
+        private bool isOpen = false;
+
+        public Canvas logbookCanvas;
+        public PauseMenu pauseMenu;
+
+        public GUI gui;
+
+        private enum Pages {
+            none = 0,
+            quests = 1,
+            map = 2
+        }
+
+        // Use this for initialization
+        private void Start() {
+        }
+
+        // Update is called once per frame
+        private void Update() {
+            if (InputManager.GetButtonDown("Map")) {
+                if (currentPage != Pages.map) {
+                    OpenToPage(Pages.map);
+                } else if (currentPage == Pages.map) {
+                    CloseLogBook();
+                }
+            } else if (InputManager.GetButtonDown("Quests")) {
+                if (currentPage != Pages.quests) {
+                    OpenToPage(Pages.quests);
+                } else if (currentPage == Pages.quests) {
+                    CloseLogBook();
+                }
+            }
+        }
+
+        private void CloseLogBook() {
+            pauseMenu.Unpause();
+            logbookCanvas.enabled = false;
+            gui.showUI = true;
+            currentPage = Pages.none;
+        }
+
+        private void OpenToPage(Pages page) {
+            gui.showUI = false;
+            pauseMenu.Pause();
+            logbookCanvas.enabled = true;
+            currentPage = page;
+        }
+    }
+}
--- Assets/Scripts/Menu Scripts/PauseMenu.cs
+++ Assets/Scripts/Menu Scripts/PauseMenu.cs
@@ -3,106 +3,107 @@ using TeamUtility.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-namespace SeaOfGreed
-{
-	public class PauseMenu : MonoBehaviour
-	{
-		public bool paused = false;
-		private Canvas canvas;
-		public Canvas optionsCanvas;
-		public Canvas gameCanvas;
-		public Canvas videoCanvas;
-		public Canvas audioCanvas;
-		public Canvas controlsCanvas;
+namespace SeaOfGreed {
 
-		void Start(){
-			canvas = GetComponent<Canvas> ();
-			canvas.enabled = false;
-			optionsCanvas.enabled = false;
-			gameCanvas.enabled = false;
-			videoCanvas.enabled = false;
-			audioCanvas.enabled = false;
-			controlsCanvas.enabled = false;
-		}
+    public class PauseMenu : MonoBehaviour {
+        public bool paused = false;
+        private Canvas canvas;
+        public Canvas optionsCanvas;
+        public Canvas gameCanvas;
+        public Canvas videoCanvas;
+        public Canvas audioCanvas;
+        public Canvas controlsCanvas;
 
-		void Update(){
-			
-			if (InputManager.GetButtonDown ("Exit")) {
-				if (!paused) {
-					Pause ();
-				} else if (canvas.enabled || optionsCanvas.enabled) {
-					Unpause ();
-				} else if (!controlsCanvas.GetComponent<ControlOptions>().isEditing){
-					ToOptions ();
-				}
-			}
-		}
+        private void Start() {
+            canvas = GetComponent<Canvas>();
+            canvas.enabled = false;
+            optionsCanvas.enabled = false;
+            gameCanvas.enabled = false;
+            videoCanvas.enabled = false;
+            audioCanvas.enabled = false;
+            controlsCanvas.enabled = false;
+        }
 
-		public void Pause(){
-			canvas.enabled = true;
-			Time.timeScale = 0f;
-			paused = true;
-		}
+        private void Update() {
+            if (InputManager.GetButtonDown("Exit")) {
+                if (!paused) {
+                    ShowPause();
+                } else if (canvas.enabled || optionsCanvas.enabled) {
+                    Unpause();
+                } else if (!controlsCanvas.GetComponent<ControlOptions>().isEditing) {
+                    ToOptions();
+                }
+            }
+        }
 
-		public void Unpause(){
-			canvas.enabled = false;
-			optionsCanvas.enabled = false;
-			gameCanvas.enabled = false;
-			videoCanvas.enabled = false;
-			audioCanvas.enabled = false;
-			controlsCanvas.enabled = false;
-			Time.timeScale = 1f;
-			paused = false;
-		}
+        private void ShowPause() {
+            canvas.enabled = true;
+            Pause();
+        }
 
-		public void Quit(){
-			GameManager.gameManager.Save ();
-			SceneManager.LoadScene ("Menu");
-		}
+        public void Pause() {
+            Time.timeScale = 0f;
+            paused = true;
+        }
 
-		public void ReturnToMainMenu(){
-			canvas.enabled = true;
-			optionsCanvas.enabled = false;
-		}
+        public void Unpause() {
+            canvas.enabled = false;
+            optionsCanvas.enabled = false;
+            gameCanvas.enabled = false;
+            videoCanvas.enabled = false;
+            audioCanvas.enabled = false;
+            controlsCanvas.enabled = false;
+            Time.timeScale = 1f;
+            paused = false;
+        }
 
-		public void GameSettings(){
-			canvas.enabled = false;
-			optionsCanvas.enabled = false;
-			gameCanvas.gameObject.GetComponent<GameOptions> ().Load ();
-			gameCanvas.enabled = true;
-		}
+        public void Quit() {
+            GameManager.gameManager.Save();
+            SceneManager.LoadScene("Menu");
+        }
 
-		public void VideoSettings(){
-			canvas.enabled = false;
-			optionsCanvas.enabled = false;
-			videoCanvas.gameObject.GetComponent<VideoOptions> ().Load ();
-			videoCanvas.enabled = true;
-		}
+        public void ReturnToMainMenu() {
+            canvas.enabled = true;
+            optionsCanvas.enabled = false;
+        }
 
-		public void AudioSettings(){
-			canvas.enabled = false;
-			optionsCanvas.enabled = false;
-			audioCanvas.gameObject.GetComponent<AudioOptions> ().Load ();
-			audioCanvas.enabled = true;
-		}
+        public void GameSettings() {
+            canvas.enabled = false;
+            optionsCanvas.enabled = false;
+            gameCanvas.gameObject.GetComponent<GameOptions>().Load();
+            gameCanvas.enabled = true;
+        }
 
-		public void ControlsSettings(){
-			canvas.enabled = false;
-			optionsCanvas.enabled = false;
-			controlsCanvas.gameObject.GetComponent<ControlOptions> ().Load ();
-			controlsCanvas.enabled = true;
-		}
+        public void VideoSettings() {
+            canvas.enabled = false;
+            optionsCanvas.enabled = false;
+            videoCanvas.gameObject.GetComponent<VideoOptions>().Load();
+            videoCanvas.enabled = true;
+        }
 
-		public void ToOptions(){
-			optionsCanvas.enabled = true;
-			gameCanvas.enabled = false;
-			videoCanvas.enabled = false;
-			audioCanvas.enabled = false;
-			controlsCanvas.enabled = false;
-			canvas.enabled = false;
+        public void AudioSettings() {
+            canvas.enabled = false;
+            optionsCanvas.enabled = false;
+            audioCanvas.gameObject.GetComponent<AudioOptions>().Load();
+            audioCanvas.enabled = true;
+        }
 
-			GameManager.gameManager.Save ();
-		}
-	}
+        public void ControlsSettings() {
+            canvas.enabled = false;
+            optionsCanvas.enabled = false;
+            controlsCanvas.gameObject.GetComponent<ControlOptions>().Load();
+            controlsCanvas.enabled = true;
+        }
+
+        public void ToOptions() {
+            optionsCanvas.enabled = true;
+            gameCanvas.enabled = false;
+            videoCanvas.enabled = false;
+            audioCanvas.enabled = false;
+            controlsCanvas.enabled = false;
+            canvas.enabled = false;
+
+            GameManager.gameManager.Save();
+        }
+    }
 }
-
