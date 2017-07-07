35c49f535950bf3a51a6d50931ba5dcf04ee15df
dadur604 <dadur604604@gmail.com>
2017-07-06 19:30:48 -0700

    yes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,40 +1,40 @@
-﻿using System.Collections;
+﻿﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.Assertions;
 using UnityEngine;
 using TeamUtility.IO;
 
 namespace SeaOfGreed{
-    public class PlayerController : MonoBehaviour {
-        
-        enum helpText
-        {
-            boardText,
-            dockText,
-            wheelText,
-            none,
-        }
+	public class PlayerController : MonoBehaviour {
 
-        helpText helpTextToDisplay = helpText.none;
+		enum helpText
+		{
+			boardText,
+			dockText,
+			wheelText,
+			none,
+		}
 
-        public GameObject boardText;
-        public GameObject dockText;
-        public GameObject wheelText;
+		helpText helpTextToDisplay = helpText.none;
 
-        public Camera mainCamera;
-        public float walkingCameraSize = 4;
-        public float interactingCameraSize = 6;
-        public float cameraEaseTime = 2;
+		public GameObject boardText;
+		public GameObject dockText;
+		public GameObject wheelText;
+
+		public Camera mainCamera;
+		public float walkingCameraSize = 4;
+		public float interactingCameraSize = 6;
+		public float cameraEaseTime = 2;
 
 		public CharacterDriver driver;
 
-        // Use this for initialization
-        void Start () {
+		// Use this for initialization
+		void Start () {
 			driver = gameObject.GetComponent<CharacterDriver> ();
-	    }    
+		}    
 
-        void walkAccordingToUserInput()
-        {
+		void walkAccordingToUserInput()
+		{
 			var input_x_right = InputManager.GetButton ("Player Right") ? 1 : 0;
 			var input_x_left = InputManager.GetButton ("Player Left") ? 1 : 0;
 			var input_y_forward = InputManager.GetButton("Player Forward") ? 1 : 0;
@@ -42,62 +42,62 @@ namespace SeaOfGreed{
 			var input_x = input_x_right - input_x_left;
 			var input_y = input_y_forward - input_y_backward;
 
-            driver.walkInDirection(new Vector3(input_x, input_y));
-        }
+			driver.walkInDirection(new Vector3(input_x, input_y));
+		}
 
-        void steerShipAccordingToUserInput()
-        {
-            driver.sprite.transform.localRotation = Quaternion.identity;
-            var shipController = driver.shipBorded.GetComponent<ShipController>();
+		void steerShipAccordingToUserInput()
+		{
+			driver.sprite.transform.localRotation = Quaternion.identity;
+			var shipController = driver.shipBorded.GetComponent<ShipController>();
 			if (InputManager.GetButton("Ship Accelerate"))
-            {
-                shipController.accelerate();
-            }
+			{
+				shipController.accelerate();
+			}
 			if (InputManager.GetButton("Ship Brake"))
-            {
-                shipController.brake();
-            }
+			{
+				shipController.brake();
+			}
 			if (InputManager.GetButton("Ship Left"))
-            {
-                shipController.turn(1);
-            }
+			{
+				shipController.turn(1);
+			}
 			if (InputManager.GetButton("Ship Right"))
-            {
-                shipController.turn(-1);
-            }
+			{
+				shipController.turn(-1);
+			}
 			if (InputManager.GetButtonDown("Use"))
-            {
+			{
 				//Debug.Log("WantsToSteeringShiptoBoardedShip");
-                driver.steeringShipToBoardedShip();
-            }
-        }
+				driver.steeringShipToBoardedShip();
+			}
+		}
 
-        void lookTowardsMouse()
-        {
-            if (driver.state == states.boardedShip || driver.state == states.onLand || driver.state == states.jumpingToLand || driver.state == states.jumpingToShip)
-            {
-                var mousePos = mainCamera.ScreenToWorldPoint(new Vector3(InputManager.mousePosition.x, InputManager.mousePosition.y, mainCamera.transform.position.z - transform.position.z));
-                Vector3 diff = (mousePos - driver.sprite.transform.position);
-                driver.lookInDirection(diff);
-            }
-        }
+		void lookTowardsMouse()
+		{
+			if (driver.state == states.boardedShip || driver.state == states.onLand || driver.state == states.jumpingToLand || driver.state == states.jumpingToShip)
+			{
+				var mousePos = mainCamera.ScreenToWorldPoint(new Vector3(InputManager.mousePosition.x, InputManager.mousePosition.y, mainCamera.transform.position.z - transform.position.z));
+				Vector3 diff = (mousePos - driver.sprite.transform.position);
+				driver.lookInDirection(diff);
+			}
+		}
 
-        void displayHelpText()
-        {
-            boardText.SetActive(false);
-            dockText.SetActive(false);
-            wheelText.SetActive(false);
+		void displayHelpText()
+		{
+			boardText.SetActive(false);
+			dockText.SetActive(false);
+			wheelText.SetActive(false);
 
-            if (driver.state == states.boardedShip && driver.canGrabWheel())
-                wheelText.SetActive(true);
-            else if (driver.state == states.onLand && driver.canBoardShip())
-                boardText.SetActive(true);
-            else if (driver.state == states.boardedShip && driver.canDockShip())
-                dockText.SetActive(true);
-        }
+			if (driver.state == states.boardedShip && driver.canGrabWheel())
+				wheelText.SetActive(true);
+			else if (driver.state == states.onLand && driver.canBoardShip())
+				boardText.SetActive(true);
+			else if (driver.state == states.boardedShip && driver.canDockShip())
+				dockText.SetActive(true);
+		}
 
-        // Update is called once per frame
-        void Update() {
+		// Update is called once per frame
+		void Update() {
 			if (Time.timeScale != 0f) {
 				if (driver.state == states.boardedShip || driver.state == states.onLand) {
 					walkAccordingToUserInput ();
@@ -119,8 +119,13 @@ namespace SeaOfGreed{
 				if (driver.state == states.boardedShip || driver.state == states.onLand || driver.state == states.jumpingToLand || driver.state == states.jumpingToShip) {
 					lookTowardsMouse ();
 				}
+				if (InputManager.GetButtonDown ("Sprint")) {
+					driver.isSprinting = true;
+				}	else if (InputManager.GetButtonUp ("Sprint")) {
+					driver.isSprinting = false;
+				}
 			}
-        }
-			
-    }
+
+		}
+	}
 }
