97e85e4efa2574e276bd3bcedd6663610ab15520
dadur604 <dadur604604@gmail.com>
2017-07-07 20:46:57 -0700

    Fixed sprite rotation

--- Assets/Scripts/CombatObjects/dumb_AI.cs
+++ Assets/Scripts/CombatObjects/dumb_AI.cs
@@ -1,24 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using SeaOfGreed;
 using UnityEngine;
-using SeaOfGreed;
+
 public class dumb_AI : MonoBehaviour {
-	private CharacterDriver driver;
-	[SerializeField] private GameObject childProjectile;
-	[SerializeField] GameObject player;
-	void Start () {
-		driver = gameObject.GetComponent<CharacterDriver>();
-	}
-	float timeSinceLastAttack = 0;
-	void Update () {
-		if((player.transform.position-transform.position).magnitude <= 5){
-			driver.lookInDirection(player.transform.position - transform.position);
-			driver.walkInDirection(player.transform.position - transform.position);
-			if(timeSinceLastAttack >= 0.5f){
-				timeSinceLastAttack = 0;
-				childProjectile.SetActive(true);
-			}
-		}
-		timeSinceLastAttack += Time.deltaTime;
-	}
+    private CharacterDriver driver;
+    [SerializeField] private GameObject childProjectile;
+    [SerializeField] private GameObject player;
+
+    private void Start() {
+        driver = gameObject.GetComponent<CharacterDriver>();
+    }
+
+    private float timeSinceLastAttack = 0;
+
+    private void Update() {
+        if ((player.transform.position - transform.position).magnitude <= 5) {
+            driver.lookInDirection(player.transform.position - transform.position);
+            driver.walkInDirection(player.transform.position - transform.position);
+            if (timeSinceLastAttack >= 0.5f) {
+                timeSinceLastAttack = 0;
+                childProjectile.SetActive(true);
+            }
+        }
+        timeSinceLastAttack += Time.deltaTime;
+    }
 }
