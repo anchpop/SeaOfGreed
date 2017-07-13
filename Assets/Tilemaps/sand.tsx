b6b41aee5ef6a1d4c228c894dd1d6e436aca2fd6
warownslife <warownslife@gmail.com>
2017-07-12 17:00:17 -0700

    Islandtest

---
+++ Assets/Tilemaps/sand.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="sand" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="../../../../../Downloads/Tiles/sand.png" width="32" height="32"/>
+</tileset>
