aa4e3f38837b2145f933e36a9bf1e235d8a3c4b1
warownslife <warownslife@gmail.com>
2017-07-13 23:00:39 -0700

    island test

---
+++ Assets/Tilemaps/untitled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Untitled" tilewidth="32" tileheight="32" tilecount="1075" columns="43">
+ <image source="../../../../../Pictures/Untitled.png" width="1392" height="816"/>
+</tileset>
