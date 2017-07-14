faae2e119f2c9431f826350a337ed236d7da2830
the-rev <reverend.ciars@gmail.com>
2017-07-13 21:50:13 -0700

    actual tileset tavern
    jesus man

---
+++ Assets/Tilemaps/untitled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="tubs_tavern_tiles" tilewidth="32" tileheight="32" tilecount="77" columns="11">
+ <image source="../../../../sea of greed/tiles/tubs_tavern_tiles.png" width="352" height="224"/>
+</tileset>
