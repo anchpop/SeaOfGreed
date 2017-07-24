1a58e21a0b076f67bf8dfbecded1d6724464fc1f
dadur604 <dadur604604@gmail.com>
2017-07-23 21:56:15 -0700

    wait for it

--- Assets/Tilemaps/untitled.tsx
+++
@@ -1,9 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<<<<<<< HEAD
-<tileset name="Untitled" tilewidth="32" tileheight="32" tilecount="1075" columns="43">
- <image source="../../../../../Pictures/Untitled.png" width="1392" height="816"/>
-=======
-<tileset name="tubs_tavern_tiles" tilewidth="32" tileheight="32" tilecount="77" columns="11">
- <image source="../../../../sea of greed/tiles/tubs_tavern_tiles.png" width="352" height="224"/>
->>>>>>> 5a7568c219016bb274176498f92f896ef328e8e2
-</tileset>
