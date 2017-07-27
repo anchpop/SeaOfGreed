e37a9a267a448ae7f485ec2ed5ef5f2ad81befb5
the-rev <reverend.ciars@gmail.com>
2017-07-27 11:51:19 -0700

    island polish

---
+++ Assets/tilesheets/grass_variation.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grass_variation" tilewidth="32" tileheight="32" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/grass_noise1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/grass_noise2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/grass_noise3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/grass_noise4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/grass_noise5.png"/>
+ </tile>
+</tileset>
