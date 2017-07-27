8a7b4eeabd2321b84a0df27b308505385626be31
the-rev <reverend.ciars@gmail.com>
2017-07-27 12:58:46 -0700

    island polish 3
    now with grass bits inside specially marked boxes

--- Assets/tilesheets/grass_variation.tsx
+++ Assets/tilesheets/grass_variation.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="grass_variation" tilewidth="32" tileheight="32" tilecount="5" columns="0">
+<tileset name="grass_variation" tilewidth="32" tileheight="32" tilecount="15" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/grass_noise1.png"/>
@@ -15,5 +15,35 @@
  </tile>
  <tile id="4">
   <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/grass_noise5.png"/>
+ </tile>
+ <tile id="5">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/beachgrass_bigcornerbottomleft.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/beachgrass_bigcornerbottomright.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/beachgrass_bigcornertopleft.png"/>
+ </tile>
+ <tile id="8">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/beachgrass_bigcornertopright.png"/>
+ </tile>
+ <tile id="9">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/beachgrass_cornerbottomleft.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/beachgrass_cornerbottomright.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/beachgrass_cornertopleft.png"/>
+ </tile>
+ <tile id="12">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/beachgrass_cornertopright.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/beachgrasssand.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/beachgrasssand4.png"/>
  </tile>
 </tileset>
