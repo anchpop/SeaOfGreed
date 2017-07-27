71b73c5f9d223e5247e95e9b03b8323661683d2a
the-rev <reverend.ciars@gmail.com>
2017-07-27 12:42:13 -0700

    island polish again

---
+++ Assets/tilesheets/taven_hut.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="cabin_tiles" tilewidth="32" tileheight="32" tilecount="130" columns="13">
+ <image source="../sprites/tiles/cabin_tiles.png" width="416" height="320"/>
+</tileset>
