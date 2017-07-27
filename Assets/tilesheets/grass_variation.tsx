8dbe4163b5387d1e70dae6594c6ab0bdb63d5b66
the-rev <reverend.ciars@gmail.com>
2017-07-27 13:50:27 -0700

    a

--- Assets/tilesheets/grass_variation.tsx
+++ Assets/tilesheets/grass_variation.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="grass_variation" tilewidth="32" tileheight="32" tilecount="15" columns="0">
+<tileset name="grass_variation" tilewidth="32" tileheight="32" tilecount="20" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/grass_noise1.png"/>
@@ -45,5 +45,20 @@
  </tile>
  <tile id="14">
   <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/beachgrasssand4.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/bits/flowers1.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/bits/flowers2.png"/>
+ </tile>
+ <tile id="17">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/bits/flowers3.png"/>
+ </tile>
+ <tile id="18">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/bits/weeds1.png"/>
+ </tile>
+ <tile id="19">
+  <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/bits/weeds2.png"/>
  </tile>
 </tileset>
