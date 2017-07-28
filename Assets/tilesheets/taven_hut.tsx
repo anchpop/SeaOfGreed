b3c232fffa2deb6278a048cb815e03c6811be287
the-rev <reverend.ciars@gmail.com>
2017-07-27 19:15:16 -0700

    island working!!
    https://www.youtube.com/watch?v=-YCN-a0NsNk

--- Assets/tilesheets/taven_hut.tsx
+++ Assets/tilesheets/taven_hut.tsx
@@ -1,4 +1,219 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="cabin_tiles" tilewidth="32" tileheight="32" tilecount="130" columns="13">
  <image source="../sprites/tiles/cabin_tiles.png" width="416" height="320"/>
+ <tile id="10">
+  <objectgroup draworder="index">
+   <object id="1" type="Ground" x="26.3636" y="-0.454545" width="6" height="33"/>
+  </objectgroup>
+ </tile>
+ <tile id="11">
+  <objectgroup draworder="index">
+   <object id="1" type="Ground" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index">
+   <object id="1" type="Ground" x="0" y="-0.363636" width="5.09091" height="32.7273"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index">
+   <object id="1" type="Ground" x="0" y="-0.363636" width="32" height="5.81818"/>
+  </objectgroup>
+ </tile>
+ <tile id="53">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="54">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="55">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="56">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="57">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="58">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="59">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="66">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="67">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="68">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="69">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="70">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="71">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="72">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="79">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="80">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="81">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="82">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="83">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="84">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="85">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="86">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="92">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="93">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="94">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="95">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="96">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="97">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="98">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="99">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="105">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="106">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="107">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="108">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="109">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="110">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="111">
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="112">
+  <objectgroup draworder="index">
+   <object id="3" type="Unpassable" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="121">
+  <objectgroup draworder="index">
+   <object id="1" type="Ground" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
 </tileset>
