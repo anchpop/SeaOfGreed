0786a1b02ef0c551fd451559a98c29abdae9c22d
the-rev <reverend.ciars@gmail.com>
2017-07-27 22:33:56 -0700

    cliffs setup

--- Assets/tilesheets/grass_variation.tsx
+++ Assets/tilesheets/grass_variation.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="grass_variation" tilewidth="64" tileheight="64" tilecount="22" columns="0">
+<tileset name="grass_variation" tilewidth="128" tileheight="128" tilecount="23" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/grass_noise1.png"/>
@@ -123,11 +123,21 @@
  </tile>
  <tile id="20">
   <image width="64" height="64" source="../sprites/palmtree.png"/>
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="15" y="9" width="30" height="41"/>
+  </objectgroup>
  </tile>
  <tile id="21">
   <image width="32" height="32" source="../sprites/revs_awful_stuff/crappy_grass/dirtpath.png"/>
   <objectgroup draworder="index">
    <object id="1" type="Ground" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="22">
+  <image width="128" height="128" source="../sprites/palmtree_big.png"/>
+  <objectgroup draworder="index">
+   <object id="1" type="Unpassable" x="29" y="16" width="64" height="58"/>
+   <object id="2" type="Unpassable" x="49" y="64" width="23" height="33"/>
   </objectgroup>
  </tile>
 </tileset>
